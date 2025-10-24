a968f00e3ad92cae3890b5880298c2531d57b2d2
outdoorsavannah <david@outdoorsavannah.com>
2025-10-24 15:54:12 -0500

    license-request: secrets need to be bound to function at build/runtime

--- functions/api/license-request.js
+++ functions/api/license-request.js
@@ -108,8 +108,8 @@ Proposed Fee: ${fee}
 `;
 
     const region = "us-east-2"; // your SES region
-    const accessKeyId = AWS_ACCESS_KEY_ID;
-    const secretKey = AWS_SECRET_ACCESS_KEY;
+    const accessKeyId = env.AWS_ACCESS_KEY_ID;
+    const secretKey = env.AWS_SECRET_ACCESS_KEY;
     const from = "david@outdoorsavannah.com";   // verified SES sender
     const to = "david@outdoorsavannah.com";     // recipient
 
