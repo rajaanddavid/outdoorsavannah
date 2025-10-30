6cdc9155c001ac716fbea6fc0c989d64151c4d4a
outdoorsavannah <david@outdoorsavannah.com>
2025-10-30 02:28:50 -0500

    mu-plugins: disable-emoji clean up

---
+++ wp-content/mu-plugins/disable-emoji.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Disable Emojis
+ * Description: Disable WordPress emoji scripts and styles
+ * Version: 1.0
+ * Author: Auto-generated
+ */
+
+function disable_emojis() {
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+}
+add_action('init', 'disable_emojis');
+
+// Remove emoji DNS prefetch
+function disable_emojis_dns_prefetch($urls, $relation_type) {
+    if ('dns-prefetch' == $relation_type) {
+        $emoji_svg_url = apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/');
+        $urls = array_diff($urls, array($emoji_svg_url));
+    }
+    return $urls;
+}
+add_filter('wp_resource_hints', 'disable_emojis_dns_prefetch', 10, 2);
